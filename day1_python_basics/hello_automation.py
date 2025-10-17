7c2189b270c72daa41e9cec269c07e86e5a7c328
Upendra58 <upendragangu4@gmail.com>
2025-10-17 10:48:16 +0530

    Day 1 - Hello

---
+++ day1_python_basics/hello_automation.py
@@ -0,0 +1,7 @@
+# day1_python_basics/hello_automation.py
+import datetime
+import os
+
+print("🚀 Automation started!")
+print("Current time:", datetime.datetime.now())
+print("Files in current directory:", os.listdir())
